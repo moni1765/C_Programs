317b9d644dee02043556a10b88b4effff62b055b
moni1765 <65708119+moni1765@users.noreply.github.com>
2020-07-25 13:15:50 +0530

    Add files via upload

---
+++ armstrong_number.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void main() {
+    int num,Num1, remainder, result = 0;
+    printf("Enter a three-digit number: ");
+    scanf("%d", &num);
+    Num1 = num;
+
+    while (Num1 != 0) {
+       
+        remainder = Num1 % 10;
+        
+       result += remainder * remainder * remainder;
+        
+       
+       Num1 /= 10;
+    }
+
+    if (result == num)
+        printf("%d is an Armstrong number.", num);
+    else
+        printf("%d is not an Armstrong number.", num);
+
+}
